6751cbdb20607189bce4c984a7d1856a3a10328d
Atul Pandit <atulpandit537@gmail.com>
2022-03-27 23:36:28 +0530

    added bubble sort

---
+++ js/sorting_algos/bubbleSort.js
@@ -0,0 +1,27 @@
+
+function Bubble(){
+    c_delay=0;
+
+    for(var i=0;i<array_size-1;i++){
+        for(var j=0;j<array_size-i-1;j++){
+            div_update(bars[j],bar_sizes[j],"rgb(233, 233, 47)");  //element to excuete
+            if(bar_sizes[j]>bar_sizes[j+1]){
+
+                div_update(bars[j],bar_sizes[j],"crimson"); 
+                div_update(bars[j+1],bar_sizes[j+1],"crimson");
+
+                var tmp=bar_sizes[j];
+                bar_sizes[j]=bar_sizes[j+1];
+                bar_sizes[j+1]=tmp;                
+
+                div_update(bars[j],bar_sizes[j],"crimson"); 
+                div_update(bars[j+1],bar_sizes[j+1],"crimson");
+            }
+            div_update(bars[j],bar_sizes[j],"rgb(115, 196, 228)");
+        }
+        div_update(bars[j],bar_sizes[j],"rgb(29, 199, 66)");
+    }
+    div_update(bars[0],bar_sizes[0],"rgb(29, 199, 66)");
+
+    enable_buttons();
+}
